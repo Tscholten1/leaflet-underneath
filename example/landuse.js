0c5bdad1a03a185b30a704da3dbf1853a314bed9
Tscholten1 <tsc244@uky.edu>
2019-11-13 08:05:17 -0600

    Update landuse.js
    Inserted personal Mapbox key in landuse.js line 16

--- example/landuse.js
+++ example/landuse.js
@@ -13,36 +13,39 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 var pois = L.tileLayer.underneath('http://{s}.tiles.mapbox.com/v4/mapbox.mapbox-streets-v8/' +
-            '{z}/{x}/{y}.vector.pbf?access_token=<your-token-here>', {
-                layers: ['landuse'],
-                lazy: true,
-                zoomIn: 0,
-                joinFeatures: true
-            })
+        '{z}/{x}/{y}.vector.pbf?access_token=pk.eyJ1IjoidHNjaG9sdGVuIiwiYSI6ImNrMnhjbGR5ZTBhNWEzbXJ2MTB6YjUwcGgifQ.rHpxZ4E3shUX1L2CEDQwXg', {
+            layers: ['landuse'],
+            lazy: true,
+            zoomIn: 0,
+            joinFeatures: true
+        })
     .addTo(map);
 
 pois
-    .on('featureadded', function(e) {
+    .on('featureadded', function (e) {
         loadedFeatures.addData(e.feature);
     })
-    .on('featurescleared', function() {
+    .on('featurescleared', function () {
         loadedFeatures.clearLayers();
     });
 
-map.on('click', function(e) {
+map.on('click', function (e) {
     var results = [],
         content = '<h2>Nearby</h2> <ul>',
-        showResults = function(results) {
+        showResults = function (results) {
             featureLayer.addData(results);
         };
 
     featureLayer.clearLayers();
-    pois.query(e.latlng, function(err, results) {
+    pois.query(e.latlng, function (err, results) {
         if (results.length > 0) {
             results = results.splice(0, 5);
             showResults(results);
         }
-    }, null, {onlyInside: true, radius: 10});
+    }, null, {
+        onlyInside: true,
+        radius: 10
+    });
 });
 
 L.popup()
